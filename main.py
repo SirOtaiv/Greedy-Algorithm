774a3b006c51868bd09b00e45d7afdeafb2fa90a
SirOtaiv <rauotavio7@gmail.com>
2025-03-12 21:43:35 -0300

    Primeiros testes de leitura e impressão dos dados do JSON

--- main.py
+++ main.py
@@ -5,7 +5,24 @@ if __name__ == "__main__":
 
     for city in list(cityObject.keys()):
         print(f'Cidade {city}')
+    
+    while True:
+        cidadeOrigem = input("Insira a Cidade Inicial\n");
 
-    cidadeOrigem = input("Insira a Cidade Inicial\n");
-    cidadeDestino = input("Insira a Cidade Desejada\n")
-    print(f'Origem: {cidadeOrigem}\n Destino: {cidadeDestino} ')
+        if cidadeOrigem in cityObject:
+            break
+        else:
+            print(f"Erro: A cidade '{cidadeOrigem}' não foi encontrada! Tente novamente.")
+
+    while True:
+        cidadeDestino = input("Insira a Cidade Desejada\n")
+
+        if cidadeDestino in cityObject:
+            break
+        else:
+            print(f"Erro: A cidade '{cidadeDestino}' não foi encontrada! Tente novamente.")
+
+    print(f'Origem: {cidadeOrigem}\nDestino: {cidadeDestino} ')
+
+    print(f'Lista de cidades próximas: {", ".join(list(cityObject[cidadeOrigem].keys()))}')
+    print(f'Lista de cidades próximas: {list(cityObject[cidadeOrigem].items())}')
