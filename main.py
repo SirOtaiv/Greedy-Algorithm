9d6e9edb7f8d8b483fdd1824f78047fddbe97150
SirOtaiv <rauotavio7@gmail.com>
2025-03-21 20:16:18 -0300

    Finalizado Algorítmo Guloso

--- main.py
+++ main.py
@@ -5,34 +5,39 @@ def Guloso_Algorythm(data: dict, origemPoint: str, destinyPoint: str):
     fullValue = 0
     keyReturn = origemPoint
     keyHistoric = [origemPoint]
+    error = None
 
     while (keyReturn != destinyPoint):
-        valueControl = 999999
+        valueControl = float('inf')
 
         for key_aux, value_aux in data[keyReturn].items():
-            if (value_aux < valueControl):
-                    
+            if (value_aux < valueControl and key_aux not in keyHistoric):
                 valueControl = value_aux
                 keyReturn = key_aux
 
-        if (keyReturn in keyHistoric):
-            print(f'Próximo caminho {keyReturn} já foi passado')
+        if valueControl == float('inf'):
+            error = "Erro: Não há caminho válido para o destino."
             break
+
+        # if (keyReturn in keyHistoric):
+        #     print(f'Próximo caminho {keyReturn} já foi passado')
+        #     break
         
-        else:
-            fullValue += valueControl
-            keyHistoric.append(key_aux)
-            print(f'Próximo destino: {keyReturn} - {valueControl}Km')
+        fullValue += valueControl
+        keyHistoric.append(keyReturn)
+        print(f'Próximo destino: {keyReturn} - {valueControl}Km')
+
+        if keyReturn not in data:
+            error = f"Erro: '{keyReturn}' não existe no dicionário de cidades."
+            break
     
-    return keyReturn, fullValue
+    return keyReturn, fullValue, error
 
 if __name__ == "__main__":
     cityObject = json.load(open("./files/cidades_sc_distancias.json", encoding="utf-8"))
 
-    print(f'O tipo da var é: {type(cityObject)}')
-
-    for city in list(cityObject.keys()):
-        print(f'Cidade {city}')
+    # for city in list(cityObject.keys()):
+    #     print(f'Cidade {city}')
     
     while True:
         cidadeOrigem = input("Insira a Cidade Inicial\n");
@@ -53,6 +58,9 @@ if __name__ == "__main__":
     print(f'Origem: {cidadeOrigem}\nDestino: {cidadeDestino} ')
 
 
-    destino, distanciaTotal = Guloso_Algorythm(cityObject, cidadeOrigem, cidadeDestino) 
+    destino, distanciaTotal, err = Guloso_Algorythm(cityObject, cidadeOrigem, cidadeDestino) 
     
-    print(f'Você chegou a seu destino {destino} - {distanciaTotal}Km Total')
+    if err is not None:
+        print(err)
+    else:
+        print(f'Você chegou a seu destino {destino} - {distanciaTotal}Km Total')
